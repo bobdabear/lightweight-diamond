b7f70a608546e664656d4d8810d0bc55237f6436
Zetta <71391363+l-Zetta-l@users.noreply.github.com>
2023-09-21 00:36:04 +0900

    Update Orderbook.sol

--- contracts/services/orderbook/Orderbook.sol
+++ contracts/services/orderbook/Orderbook.sol
@@ -15,7 +15,7 @@ contract Orderbook is DiamondFacade {
     /*
      * The address of '_app' is the address of the parent diamond. '_app' has all the facet contracts of the corresponding facade,
      * and in order to refer to them in the 'fallback', the app (parent diamond contract) address is
-     * passed as a parameter here when the first contract is created.
+     * passed as a parameter here when the contract's first creation.
      */
 
     constructor(
@@ -25,7 +25,7 @@ contract Orderbook is DiamondFacade {
         address _app
     ) DiamondFacade(keccak256("orderbook.diamond"), _app) {
         // It can also be used in other ways such as shown below in other facet contracts.
-        // Data.Storage storage Data.load();
+        // Data.Storage storage $ = Data.load();
 
         $.base = _base;
         $.quote = _quote;
